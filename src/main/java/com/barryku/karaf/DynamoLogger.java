def40c19de7c8e285f262465bc7cad6953a5c7b5
Barry Ku <barryku@tcl.com>
2016-02-14 16:49:36 -0800

    fixed dependency issue

--- src/main/java/com/barryku/karaf/DynamoLogger.java
+++ src/main/java/com/barryku/karaf/DynamoLogger.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import com.amazonaws.regions.Region;
+import com.amazonaws.regions.RegionUtils;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -13,7 +14,6 @@ import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 
 public class DynamoLogger {
 
@@ -21,15 +21,16 @@ public class DynamoLogger {
 	private DynamoDB db;
 
 	public DynamoLogger(Region region) {
-		//AmazonDynamoDB dynamo = new AmazonDynamoDBClient(AwsCredentialFactory.getCredential());
-		AmazonDynamoDB dynamo = new AmazonDynamoDBClient();
+		AmazonDynamoDB dynamo = new AmazonDynamoDBClient(AwsCredentialFactory.getCredential());
+		//AmazonDynamoDB dynamo = new AmazonDynamoDBClient();
 		dynamo.setRegion(region);
 		db = new DynamoDB(dynamo);
 	}
 
 	public List<Item> getLog(String jobId) {
-		
+		System.out.println("get logs:" + jobId + "~");
 		Table table = db.getTable(LOG_TABLE);
+	
 
 		QuerySpec spec = new QuerySpec()
 		    .withHashKey("jobId", jobId);
@@ -48,4 +49,9 @@ public class DynamoLogger {
 		return result;
 	}
 
+	public static void main(String[] args) {
+		DynamoLogger logger = new DynamoLogger(RegionUtils.getRegion("us-west-2"));
+		List<Item> s = logger.getLog("test");
+		System.out.print(s);
+	}
 }
